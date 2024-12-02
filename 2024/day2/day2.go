62bc40759d4691b1d0b2dcf2fa0f969f5d65c386
Brian <brianfransis.moniaga@gmail.com>
2024-12-01 23:50:59 -0700

    fmt

--- 2024/day2/day2.go
+++ 2024/day2/day2.go
@@ -30,37 +30,37 @@ func Run() {
 			nums[i] = num
 		}
 
-    if isSafe(nums) {
+		if isSafe(nums) {
 			ans++
 		} else {
-      // Part two.
-      for i := range nums {
-        cpy_nums := make([]int, len(nums))
-        copy(cpy_nums, nums)
-        slices.Delete(cpy_nums, i, i+1)
-        cpy_nums = cpy_nums[:len(cpy_nums)-1]
-        if isSafe(cpy_nums) {
-          ans++
-          break
-        }
-      }
-    }
+			// Part two.
+			for i := range nums {
+				cpy_nums := make([]int, len(nums))
+				copy(cpy_nums, nums)
+				slices.Delete(cpy_nums, i, i+1)
+				cpy_nums = cpy_nums[:len(cpy_nums)-1]
+				if isSafe(cpy_nums) {
+					ans++
+					break
+				}
+			}
+		}
 	}
 
 	fmt.Println(ans)
 }
 
 func isSafe(nums []int) bool {
-	n := len(nums)	
+	n := len(nums)
 
 	prev := nums[0]
 	decreasing := nums[1] < nums[0]
 
 	for i := 1; i < n; i++ {
 		diff := nums[i] - prev
-		if (decreasing &&  (diff >= 0 || -diff > 3)) ||
-			 (!decreasing && (diff <= 0 || diff > 3)) {
-				return false
+		if (decreasing && (diff >= 0 || -diff > 3)) ||
+			(!decreasing && (diff <= 0 || diff > 3)) {
+			return false
 		}
 		prev = nums[i]
 	}
